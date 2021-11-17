0fc06d3481ea7c6310b797cf43fbb85a8b030786
vsinky <sinkykumariverma@gmail.com>
2021-11-17 19:49:00 +0530

    Added: create generic class

--- src/com/bridgelabz/generics/MaximumNumber.java
+++ src/com/bridgelabz/generics/MaximumNumber.java
@@ -13,16 +13,20 @@ public class MaximumNumber {
 
 		System.out.println("Maximum ::" + max);
 	}
-
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generic Program.");
+		System.out.println("Show Maximum Value:");
 		Integer x = 10, y = 23, z = 15;
 		Float d = 12.5f, e = 13.4f, f = 22.0f;
 		String s1 = "Apple", s2 = "Peach", s3 = "Banana";
-
+		
 		MaximumNumber max = new MaximumNumber();
 		max.getMaximum(x, y, z);
 		max.getMaximum(d, e, f);
 		max.getMaximum(s1, s2, s3);
+		
+		
 	}
 }
