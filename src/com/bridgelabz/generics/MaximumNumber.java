a6dc67911e161d2a4bd6ca06bb902aea616afdd2
vsinky <sinkykumariverma@gmail.com>
2021-11-17 20:29:05 +0530

    Added: generic method

--- src/com/bridgelabz/generics/MaximumNumber.java
+++ src/com/bridgelabz/generics/MaximumNumber.java
@@ -1,47 +1,52 @@
 package com.bridgelabz.generics;
 
-public class MaximumNumber {
-	private static <T extends Comparable> void getMaximum(T x, T y, T z) {
+public class MaximumNumber<T extends Comparable<T>> {
 
-		T max = x;
-		if (y.compareTo(x) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
+	T x, y, z;
 
-		System.out.println("Maximum ::" + max);
+	public void MaximumCheck(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 
-	public static <T extends Comparable<T>> T checkMaximum_With_MoreThan_ThreeParameters(T... elements) {
-		T max = elements[0];
-		for (T maximumParametereter : elements) {
-			if (maximumParametereter.compareTo(max) > 0) {
-				max = maximumParametereter;
-			}
+	// determine the largest of three Comparable Objects
+	public static <T extends Comparable<T>> T checkMaximum(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0) {
+			max = y; // y is largest thus for
 		}
-		return max;
+		if (z.compareTo(max) > 0) {
+			max = z; // z is largest now
+		}
+		printMax(max);
+		return max; // returns maximum
+	}
+
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;
+		if (y.compareTo(max) > 0) {
+			max = y; // y is the largest so far
+		}
+		if (z.compareTo(max) > 0) {
+			max = z; // z is the largest now
+		}
+		printMax(max);
+		return max; // returns the largest so for
+	}
+
+	public static <T> void printMax(T max) {
+		System.out.println("print maximum : " + max);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Generic Program.");
-		System.out.println("Show Maximum Value:");
-		Integer x = 10, y = 23, z = 15;
-		Float d = 12.5f, e = 13.4f, f = 22.0f;
-		String s1 = "Apple", s2 = "Peach", s3 = "Banana";
-		Integer i = checkMaximum_With_MoreThan_ThreeParameters(10, 23, 15, 20, 24);
-		Float c = checkMaximum_With_MoreThan_ThreeParameters(12.5f, 13.4f, 22.0f, 24.0f, 20.8f);
-		String s = checkMaximum_With_MoreThan_ThreeParameters("Apple", "Peach", "Banana", "Mango", "Orange");
+		Integer n = checkMaximum(10, 23, 15);
+		Float d = checkMaximum(12.5f, 13.4f, 22.0f);
+		String s = checkMaximum("Apple", "peach", "Banana");
 
-		MaximumNumber max = new MaximumNumber();
-		max.getMaximum(x, y, z);
-		max.getMaximum(d, e, f);
-		max.getMaximum(s1, s2, s3);
-
-		System.out.println("The maximum integer value is : " + i);
-		System.out.println("The maximum float value is : " + c);
-		System.out.println("The maximum string value is : " + s);
-
+		printMax(n);
+		printMax(d);
+		printMax(s);
 	}
 }
+ 
